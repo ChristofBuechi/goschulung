7f55ac4e898c683ad05ec2cfa7c90ada15a6fa01
Christof Buechi <christof.buechi@gmail.com>
2016-04-25 13:28:09 +0200

    readFile example

--- readFile/src/main/readfile.go
+++ readFile/src/main/readfile.go
@@ -2,23 +2,41 @@ package main
 
 import ("fmt"
 	"io/ioutil"
+	"strings"
+	"os"
 )
 
 var (
-	byteArray[]byte
 	line string
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 
 func main() {
 	fmt.Println("read from file")
 
-	byteArray, _ = ioutil.ReadFile("/etc/fstab");
+	byteArray, err := ioutil.ReadFile("/etc/fstab");
+	check(err)
 	line = string(byteArray[:])
+
+	words := strings.Split(line, " ")
+	for _,w := range words {
+		fmt.Printf("%s \n", w)
+	}
 
 	fmt.Println(line)
 
 
+	fmt.Println("\nos open\n")
+	f, err := os.Open("/etc/fstab")
+	check(err)
 
+	b1 := make([]byte, 1000)
+	n1, err := f.Read(b1)
+	fmt.Printf("%d bytes: %s\n", n1, string(b1))
 }
