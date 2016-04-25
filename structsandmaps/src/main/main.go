6e225af50940477becc2a5059d2e1ed9f1a6a1a3
Christof Buechi <christof.buechi@gmail.com>
2016-04-25 11:37:13 +0200

    add structsamples

--- structsandmaps/src/main/main.go
+++ structsandmaps/src/main/main.go
@@ -4,10 +4,28 @@ import (
 	"fmt"
 )
 
-
-
+var (
+	mySlice[]int
+	myMap map[string]int
+)
 
 func main() {
+	myMap := make(map[string]int)
+	myMap["zero"] = 0
+	myMap["first"] = 1
+	myMap["second"] = 2
 
+	for key, val := range myMap {
+		fmt.Printf("%s: %d\n", key, val)
+	}
+
+	val := myMap["second"]
+	fmt.Printf("The value is %d\n", val)
+
+	unknown, found := myMap["unknwon"]; if found {
+		fmt.Printf("The value is %d\n", unknown)
+	} else {
+		fmt.Println("The value is not found")
+	}
 }
 
