c76aa123c74dbcec93368ee7da8aedbc06e8d2e0
Christof Buechi <christof.buechi@gmail.com>
2016-04-25 11:26:00 +0200

    refactor and cleanup

--- defer/src/main/main.go
+++ defer/src/main/main.go
@@ -18,12 +18,31 @@ func findMax(a []int) (max int) {
 	return
 }
 
+func findMaxSpecial(a []int) (max int) {
+	defer func(firstMax int) {
+		fmt.Printf("First-Max: %d, Curr-Max: %d\n", firstMax, max)
+		max = max * 2
+	}(max)
+	for _,v := range a{
+		if v > max {
+			max = v
+		}
+	}
+	return
+}
+
 
 func main() {
 
 	//named return value: max
 	max := findMax([] int {1,2,3,4,99,5})
 	fmt.Printf("Biggest value:  %d\n", max)
+	fmt.Println("\n")
+
+	//named return with defer: max
+	maxSpecial := findMaxSpecial([] int {1,2,3,4,99,5})
+	fmt.Printf("Max val:  %d\n", maxSpecial)
+	fmt.Println("\n")
 
 
 	//defer example - defer is called late, but with the value in order
