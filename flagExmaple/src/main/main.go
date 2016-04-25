352c00aff559ad96782b04bbb13e7ca5a9e79e2d
Christof Buechi <christof.buechi@gmail.com>
2016-04-25 10:28:22 +0200

    added a switch to flag example

--- flagExmaple/src/main/main.go
+++ flagExmaple/src/main/main.go
@@ -14,7 +14,18 @@ func main() {
 	flag.Parse()
 
 	if *myname == "" {
-		log.Fatal("Hey.. you didn't tell me your name")
+		log.Fatal("Hey.. you didn't tell me your name\n")
+	}
+
+	if len(*myname) < 2 {
+		log.Fatal("I dont trust you\n")
+	}
+
+	switch *myname {
+	case "christof":
+		fmt.Print("welcome back christof\n")
+	case "unknown":
+		fmt.Print("you are unknown - really ?\n")
 	}
 	fmt.Printf("name has value %s!\n", *myname)
 
